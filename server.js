79823c24c1cf2cfec842a75b2fec7ae6804874ed
fionato <storeforphuong@gmail.com>
2019-09-11 10:13:00 -0400

    update client proxy

--- server.js
+++ server.js
@@ -41,10 +41,13 @@ app.use(
 
 if (process.env.NODE_ENV === "production") {
   // set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+  // app.get('*', (req, res) => {
+  //   res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  // });
 }
 
 const port = process.env.PORT || 4000;
